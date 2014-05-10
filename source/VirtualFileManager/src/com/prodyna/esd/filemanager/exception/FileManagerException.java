d1adce9de6678519a4dae5564156e386d9188536
Oliver Teichmann <oteichmann@prodyna.com>
2014-05-10 10:46:41 +0200

    Validating input of addNode

--- source/VirtualFileManager/src/com/prodyna/esd/filemanager/exception/FileManagerException.java
+++ source/VirtualFileManager/src/com/prodyna/esd/filemanager/exception/FileManagerException.java
@@ -3,4 +3,27 @@ package com.prodyna.esd.filemanager.exception;
 public class FileManagerException extends Exception {
 
 	private static final long serialVersionUID = 8347776987747683873L;
+
+	public FileManagerException() {
+		super();
+	}
+
+	public FileManagerException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public FileManagerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public FileManagerException(String message) {
+		super(message);
+	}
+
+	public FileManagerException(Throwable cause) {
+		super(cause);
+	}
+	
+	
 }
