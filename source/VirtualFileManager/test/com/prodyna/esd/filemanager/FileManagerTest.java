25c45ec1170fac3c9d52e794889f3768f167bb84
Reiner Hoppe <reiner.hoppe@prodyna.com>
2014-05-10 11:41:26 +0200

    no message

--- source/VirtualFileManager/test/com/prodyna/esd/filemanager/FileManagerTest.java
+++ source/VirtualFileManager/test/com/prodyna/esd/filemanager/FileManagerTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import com.prodyna.esd.filemanager.exception.FileManagerException;
 import com.prodyna.esd.filemanager.model.Directory;
+import com.prodyna.esd.filemanager.model.File;
 import com.prodyna.esd.filemanager.model.Node;
 
 public class FileManagerTest {
@@ -32,7 +33,9 @@ public class FileManagerTest {
 			assertNotNull(rootNode);
 			assertNotNull(rootNode.getMetaData());
 			assertNotNull(rootNode.getChildren());
+			assertEquals(true, rootNode.isRoot());
 			assertEquals("A created root node should never change", rootNode, fileManager.rootNode());
+			assertEquals("A created root node should never change", true, rootNode == fileManager.rootNode());
 		} catch (FileManagerException e) {
 			fail("No exception was expected");
 		}
@@ -40,8 +43,32 @@ public class FileManagerTest {
 
 	@Test
 	public void testAddNodeForMemoryFileManager() {		
-		Directory rootNode = null;
-		Node childNode = null;
+		try {
+			Directory rootNode = fileManager.rootNode();
+			File fileNode = new File("Test1.txt");
+			
+			File addedFileNode = fileManager.addNode(rootNode, fileNode);			
+			assertNotNull(addedFileNode);
+			assertEquals(fileNode.getName(), addedFileNode.getName());
+			assertEquals("Added files shouldn't be the same files", false, fileNode == addedFileNode);
+					
+			Directory directoryNode = new Directory("Test");
+			Directory addedDirectoryNode = fileManager.addNode(rootNode, directoryNode);
+			
+			assertNotNull(addedDirectoryNode);
+			assertEquals(false, addedDirectoryNode.isRoot());
+			assertEquals(addedDirectoryNode.getName(), addedDirectoryNode.getName());
+			assertEquals("Added directories shouldn't be the same directories", false, directoryNode == addedDirectoryNode);
+			
+			assertEquals(rootNode.getChildren().size(), 2);
+			
+			assertNotNull(rootNode.getMetaData());
+			assertNotNull(rootNode.getChildren());
+			assertEquals("A created root node should never change", rootNode, fileManager.rootNode());
+			assertEquals("A created root node should never change", true, rootNode == fileManager.rootNode());
+		} catch (FileManagerException e) {
+			fail("No exception was expected");
+		}
 	}
 
 	@Test
@@ -58,13 +85,13 @@ public class FileManagerTest {
 		try {			
 			fileManager.addNode(rootNode, null);
 			
-			fail("An exception was exspected");
+			fail("An exception was expected");
 		} catch (FileManagerException exception) { }
 		
 		try {			
 			fileManager.addNode(null, childNode);
 			
-			fail("An exception was exspected");
+			fail("An exception was expected");
 		} catch (FileManagerException exception) { }
 	}
 }
